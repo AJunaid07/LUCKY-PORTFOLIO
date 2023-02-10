0150bb45315f67303f26f53f772f041e5b43313a
AJunaid07 <rajali069@gmail.com>
2023-02-10 12:32:48 +0530

    Updates

---
+++ precache-manifest.fe9207e0b44aa7f7b135941d70f4b4b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b4b040b17ae6367377c583a96af04e9",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/index.html"
+  },
+  {
+    "revision": "1d49b9a950ad2f6ea3b5",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/css/2.f00fa05b.chunk.css"
+  },
+  {
+    "revision": "94fbddc2e7b3e619fa07",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/css/main.67baed01.chunk.css"
+  },
+  {
+    "revision": "1d49b9a950ad2f6ea3b5",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/js/2.df0b10ff.chunk.js"
+  },
+  {
+    "revision": "00805778243e590c969fe4f0c68d71f6",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/js/2.df0b10ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94fbddc2e7b3e619fa07",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/js/main.4129e26a.chunk.js"
+  },
+  {
+    "revision": "0700e6fa83288221f1ce",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/js/runtime-main.ab80bd93.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "6c4f01d3be48a7463b561f39811d0fb2",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/media/img.6c4f01d3.jpg"
+  },
+  {
+    "revision": "87561258a0fb17472008a4b2ff66696f",
+    "url": "/AJunaid07.github.io/lUCKY-PORTFOLIO/static/media/laptop1.87561258.jpg"
+  }
+]);
